f8cff9b9742ebe71b99698d9ab25d86b66e481f8
mahesh <maheshverma.niit@gmail.com>
2023-08-28 01:07:24 +0530

    save

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -30,7 +30,7 @@ const NavBar = () => {
             </nav>
             <div className={`navbar ${stickyClass}`}>
                 <nav class="navbar navbar-expand-lg  " >
-                    <div class="container-fluid">
+                    <div class="container">
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
